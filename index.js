411c8760edf57b56ab1bfca397c84524a929c3b4
nghiendv2 <nghiendv2@msb.com.vn>
2024-05-02 16:21:52 +0700

    add logic shell

--- index.js
+++ index.js
@@ -2,8 +2,48 @@
  * @format
  */
 
-import {AppRegistry} from 'react-native';
+import {ScriptManager, Script, Federated} from '@callstack/repack/client';
+import {AppRegistry, Platform} from 'react-native';
 import App from './App';
+import getContainersURL from './src/utils/getContainersURL';
 import {name as appName} from './app.json';
+import {version as appVersion} from './package.json';
+
+ScriptManager.shared.addResolver(async (scriptId, caller) => {
+  const containersURL = getContainersURL({
+    hostname: process.env.SAS_CATALOG_SERVER_URL,
+    version: appVersion,
+    platform: Platform.OS,
+    appName: 'host', // for testing purposes
+  });
+
+  const containersResponse = await fetch(containersURL);
+
+  const containers = await containersResponse.json();
+
+  const resolveURL = Federated.createURLResolver({
+    containers,
+  });
+
+  let url;
+  if (caller === 'main') {
+    url = Script.getDevServerURL(scriptId);
+  } else {
+    url = resolveURL(scriptId, caller);
+  }
+
+  if (!url) {
+    return undefined;
+  }
+
+  return {
+    url,
+    cache: false,
+    query: {
+      platform: Platform.OS,
+    },
+    verifyScriptSignature: 'off',
+  };
+});
 
 AppRegistry.registerComponent(appName, () => App);
